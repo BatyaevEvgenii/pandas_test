82d1a40fb43212db92ef6904f89ea4dfca90dc71
Eugene <marat@192-168-1-105.local>
2020-01-15 21:32:43 +0300

    up

--- index.py
+++ index.py
@@ -12,6 +12,7 @@ spend = data_p.query('spend > 0')
 # new_df = pd.merge(spend, installs, how='inner')
 # or(по умолчанию берется INNER)
 new_df = spend.merge(installs)
+
 new_df['cpi'] = new_df['spend'] / new_df['installs']
-all = pd.DataFrame(new_df, columns=['app', 'date', 'campaign', 'os', 'installs', 'spend', 'cpi'])
-all.to_csv('out.csv', columns=['app', 'date', 'campaign', 'os', 'installs', 'spend', 'cpi'], index = False)
+
+new_df.to_csv('out.csv', columns=['app', 'date', 'campaign', 'os', 'installs', 'spend', 'cpi'], index=False)
